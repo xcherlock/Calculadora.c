644ab7659395defbb7112540ce4c8158e1e51adf
vec70r <116394138+vec70rr@users.noreply.github.com>
2024-02-23 20:08:30 -0600

    Update calculadora.c
    Se agregó la función de resta y la función clean() que limpia la pantalla despues de cada operación

--- calculadora.c
+++ calculadora.c
@@ -5,11 +5,13 @@ int suma(int a, int b);
 int resta(int a, int b);
 int multiplicacion(int a, int b);
 int division(int a, int b);
+void clean();
 
 int main(){
     int menu = 1;
     int sel = 0, a, b; 
-    while(menu){   
+    while(menu==1){
+		clean();   
         printf("\n-------- Bienvenido esta es una calculadora --------");
         printf("\nPuedes realizar las operaciones de: ");
         printf("\n1. Suma");
@@ -19,19 +21,33 @@ int main(){
         printf("\n5. Salir");
         printf("\n----> Tu seleccion: ");
         scanf("%d", &sel);
-        printf("\n\nIngresa el primer numero: ");
-        scanf("%d", &a);
-        printf("\nIngresa el segundo numero: ");
-        scanf("%d", &b);
+       
         switch(sel){
             case 1:
+            	printf("\n\nIngresa el primer numero: ");
+        		scanf("%d", &a);
+        		printf("\nIngresa el segundo numero: ");
+        		scanf("%d", &b);
                 printf("\n.... El resultado de la suma es %d", suma(a, b));
                 break;
             case 2:
+            	printf("\n\nIngresa el primer numero: ");
+        		scanf("%d", &a);
+        		printf("\nIngresa el segundo numero: ");
+        		scanf("%d", &b);
+        		 printf("\n.... El resultado de la resta es %d", resta(a, b));
                 break;
             case 3:
+            	printf("\n\nIngresa el primer numero: ");
+        		scanf("%d", &a);
+        		printf("\nIngresa el segundo numero: ");
+        		scanf("%d", &b);
                 break;
             case 4:
+            	printf("\n\nIngresa el primer numero: ");
+        		scanf("%d", &a);
+        		printf("\nIngresa el segundo numero: ");
+        		scanf("%d", &b);
                 break;
             case 5:
                 break;
@@ -50,9 +66,17 @@ int suma(int a, int b){
     return a + b;
 }
 int resta(int a, int b){
-    return 0;
+    return a - b;
 }
 int multiplicacion(int a, int b){
     return 0;}
 int division(int a, int b){
     return 0;}
+    
+void clean(){
+	#ifdef _WIN32
+		system("cls");
+	#else
+		system("clear");
+	#endif
+}
